a631fcd89b42ea55f80ecad73bda8a03d9cd42e9
Shaan Sundar <shaan.idtindia@gmail.com>
2022-01-27 21:24:02 +0530

    some additions

--- frontend-analytics/src/App.js
+++ frontend-analytics/src/App.js
@@ -1,9 +1,22 @@
-
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
+    <div className="bg-black h-screen w-screen flex flex-col items-center justify-start">
+      <header className="flex text-white items-center justify-between w-full h-20 p-6">
+        <h1 className="text-3xl">
+          Taxable Tx
+        </h1>
+        <h1 className="font-bold">
+          Analytics Dashboard
+        </h1>
+        <button className="w-36 h-10 bg-yellow-600">
+          Connect Wallet
+        </button>
+      </header>
+      <section className="flex items-center justify-start p-5 w-full h-full">
+        <div className="rounded-xl bg-yellow-600 w-1/2 h-full">
+
+        </div>
+      </section>
     </div>
   );
 }
